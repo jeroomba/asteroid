abfd6149b1096eacfbec35bb5603822cbdd32cf7
Jerome <jerome.greutmann@protonmail.com>
2026-09-18 15:33:09 +0200

    added fps, and drew the player record

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame verision: {pygame.version.ver}")
     print(f"Screen width: 1280")
     print(f"Screen height: 720")
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
